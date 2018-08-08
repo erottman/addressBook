e9c43234cb96291cf0c1b5318b75b491a660ff31
Erica Rottman <ericarottman@Ericas-MBP.localdomain>
2018-08-08 09:12:28 -0700

    migrations

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+const methodOverride = require('method-override');
+const hbs = require('hbs');
+const hbsUtils = require('hbs-utils')(hbs);
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var addresses = require('./routes/addresses');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+hbsUtils.registerPartials(path.join(__dirname, 'views'), {
+  match: /\/?.*_.*\.(html|hbs)$/,
+  name: (name) => {
+    let pathArr = name.split('/')
+    let last = pathArr.length - 1
+    pathArr[last] = pathArr[last].slice(1)
+    let newName = pathArr.join('/')
+
+    return newName
+  }
+})
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +37,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/contacts', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
